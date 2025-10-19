55d57101bf15b0bfdbe7a830ebad19fbe8ee5e59
sunil <sunildubey198713@gmail.com>
2025-10-20 02:59:53 +0530

    rg added

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,3 +13,9 @@ resource "azurerm_resource_group" "rg2" {
   location = "West Europe"
 }
 
+
+resource "azurerm_resource_group" "rg3" {
+  name     = "example-rg334"
+  location = "West Europe"
+}
+
